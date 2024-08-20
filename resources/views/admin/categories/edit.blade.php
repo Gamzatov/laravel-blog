8ddc4f8618e94a1c0fd34e38e23ca17df6920292
Dirik <dirik@Diriks-MacBook-Pro.local>
2024-08-20 12:09:45 +0300

    fixed tags buttons

--- resources/views/admin/categories/edit.blade.php
+++ resources/views/admin/categories/edit.blade.php
@@ -6,18 +6,21 @@
         <div class="form-group">
             <label for="">Name</label>
             <input class="form-control" type="text" name="name" value="{{$category->name}}">
-        </div>
-        <div class="form-group my-2">
-            <button class="btn btn-dark mx-1">
-                Save
-            </button>
-            <form action="{{route('admin.categories.destroy', compact('category'))}}" method="post">
-                @csrf
-                @method('delete')
-                <button class="btn btn-dark mx-1">
-                    Delete
-                </button>
-            </form>
+
         </div>
     </form>
+    <div class="form-group my-2">
+        <button class="btn btn-dark mx-1">
+            Save
+        </button>
+        <button form="deleteTag" class="btn btn-dark mx-1">
+            Delete
+        </button>
+        <form id="deleteTag" action="{{route('admin.categories.destroy', compact('category'))}}" method="post">
+            @csrf
+            @method('delete')
+
+        </form>
+    </div>
+
 </x-admin.layout>
